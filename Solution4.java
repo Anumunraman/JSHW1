b648b38b2c1186b4283bb2bd3c7e55e9ceb2be55
nikolaev mikhail <6n2n69@gmail.com>
2022-12-11 01:08:04 +0300

    added task5

---
+++ Solution4.java
@@ -0,0 +1,8 @@
+public class Solution4 {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
+}
+
